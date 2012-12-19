33c434b24a8056e52a0741d6f5ba98a472fb4728
Carlos Sanchez <csanchez@maestrodev.com>
2012-12-19 13:11:22 +0100

    Add architecture to centos facts for specs

--- spec/support/centos.rb
+++ spec/support/centos.rb
@@ -5,6 +5,7 @@ def centos_facts
     :kernel          => 'Linux',
     :operatingsystem => 'CentOS',
     :osfamily        => 'RedHat',
-    :postgres_default_version => '8.4' # CentOS 6.3
+    :postgres_default_version => '8.4', # CentOS 6.3
+    :architecture    => 'x86_64'
   }
 end
