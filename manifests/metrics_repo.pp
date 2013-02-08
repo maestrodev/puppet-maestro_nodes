b26a7b7c03d092a4a08f1892eca5e423f1cb6dc4
Etienne Pelletier <epelletier@maestrodev.com>
2013-02-08 11:06:24 -0600

    Disabled mongo backend for statsd because it was causing the daemon to crash.

--- manifests/metrics_repo.pp
+++ manifests/metrics_repo.pp
@@ -1,24 +1,31 @@
 class maestro_nodes::metrics_repo{
-  
-  
+
+  include nodejs
+
+
   class { 'mongodb':
     enable_10gen => true,
   }
+
+  # Mongo backend is disabled temporarily due to an
+  # unexplained bug that crashes the statsd daemon.
+  #  $statsd_config = { 'mongoHost' => '"localhost"',
+  #    'mongoMax' => 2160
+  #  }
+  #  $backends =   [ 'mongo-statsd-backend' ]
+
+  $statsd_config = { }
+  $backends = [ ]
   
-  $statsd_config = { 'mongoHost' => '"localhost"',
-                     'mongoMax' => 2160
-                   }
-  
-  class { 'nodejs':
-  
-  } ->
+
+#  package { 'mongo-statsd-backend':
+#    ensure   => present,
+#    provider => npm,
+#  } ->
   class { 'statsd':
-    backends => [ 'mongo-statsd-backend' ],
+    backends => $backends,
     config => $statsd_config,
-  } ->
-  package { 'mongo-statsd-backend':
-      ensure   => present,
-      provider => npm,
   }
+
 
 }
