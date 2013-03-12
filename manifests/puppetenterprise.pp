cdd717afc14e1dd724149ac0d9e5bb2f7f5d565b
Etienne Pelletier <epelletier@maestrodev.com>
2013-03-12 10:18:52 -0500

    Added some specs to test the new puppetenterprise class.

--- manifests/puppetenterprise.pp
+++ manifests/puppetenterprise.pp
@@ -1,6 +1,7 @@
 class maestro_nodes::puppetenterprise($repo = $maestro::repository::maestrodev, $stomp_port = 61630) {
 
   $srcdir = '/usr/local/src'
+  $unpackdir = "${srcdir}/puppet-enterprise-2.7.1-el-6-x86_64"
 
   wget::authfetch { 'fetch-pe':
     user        => $repo['username'],
@@ -10,22 +11,22 @@ class maestro_nodes::puppetenterprise($repo = $maestro::repository::maestrodev, 
   } ->
   exec { 'unpack-pe':
     command => 'tar -xvf puppet-enterprise-el-6-x86_64-2.7.1.tar.gz',
-    cwd => $srcdir,
-    creates => '/usr/local/src/puppet-enterprise-2.7.1-el-6-x86_64',
+    cwd     => $srcdir,
+    creates => $unpackdir,
   } ->
-  file { '/usr/local/src/puppet-enterprise-2.7.1-el-6-x86_64/puppet-enterprise-installer-answers.txt':
+  file { "${unpackdir}/puppet-enterprise-installer-answers.txt":
     source => 'puppet:///modules/maestro_nodes/puppet-enterprise-installer-answers.txt',
   } ->
   exec { 'install-pe':
-    command => '/usr/local/src/puppet-enterprise-2.7.1-el-6-x86_64/puppet-enterprise-installer -a puppet-enterprise-installer-answers.txt',
-    cwd => '/usr/local/src/puppet-enterprise-2.7.1-el-6-x86_64',
+    command => "${unpackdir}/puppet-enterprise-installer -a puppet-enterprise-installer-answers.txt",
+    cwd     => $unpackdir,
     timeout => 0,
     creates => '/opt/puppet',
   } ->
   file { '/etc/puppetlabs/facter/facts.d/puppet_enterprise_installer.txt':
-    content => template("maestro_nodes/puppet_enterprise_installer.txt.erb"),
+    content => template('maestro_nodes/puppet_enterprise_installer.txt.erb'),
   } ->
-  exec { '/pe-puppet-agent':
+  exec { 'pe-puppet-agent':
     command => '/opt/puppet/bin/puppet agent --test',
     returns => [0, 2],
   }
