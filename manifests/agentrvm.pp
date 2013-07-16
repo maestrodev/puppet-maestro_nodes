dc0bc1eba0501f80d60bd7e25ad1e6de417e3408
Carlos Sanchez <csanchez@maestrodev.com>
2013-07-16 09:30:37 +0200

    Don't install any rvm rubies by default, compositions can do that

--- manifests/agentrvm.pp
+++ manifests/agentrvm.pp
@@ -7,12 +7,7 @@ class maestro_nodes::agentrvm(
   # rvm needs to be included after other classes that install packages that rvm also installs
   include rvm
 
-  rvm::system_user { $agent_user: } ->
-  rvm_system_ruby { 'ruby-1.8.7-p352':
-    ensure      => 'present',
-    default_use => true,
-    require     => Package['git'],
-  }
+  rvm::system_user { $agent_user: }
 
   # if we have a epel stage defined in parent nodes ensure it runs before installing rvm
   # as some rvm dependencies are in epel repositories
