bc8f727c9f73171ef44c4d60cb24ac77ea4a1bc3
Carlos Sanchez <csanchez@maestrodev.com>
2013-09-11 17:12:59 +0200

    Use hiera-puppet-helper to create mock hiera data

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 group :rake do
   gem 'puppet',       '>=3.0.1'
   gem 'rspec-puppet', '>=0.1.3'
+  gem 'hiera-puppet-helper'
   gem 'rake',         '>=0.9.2.2'
   gem 'puppet-lint',  '>=0.1.12'
   gem 'puppetlabs_spec_helper'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,24 @@
 Dir["./spec/support/**/*.rb"].each {|f| require f}
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'hiera-puppet-helper/rspec'
+require 'hiera'
+require 'puppet/indirector/hiera'
+
+# config hiera to work with let(:hiera_data)
+def hiera_stub
+  config = Hiera::Config.load(hiera_config)
+  config[:logger] = 'puppet'
+  Hiera.new(:config => config)
+end
 
 RSpec.configure do |c|
+  c.mock_framework = :rspec
   c.include MaestroNodes::CentOS
 
-  c.before(:all) do
-    # Using Puppet 3 configure hiera
-    if Integer(Puppet.version.split('.').first) >= 3
-      hiera_config = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'hiera.yaml'))
-      raise "Hiera config file does not exist: #{hiera_config}" unless File.exists? hiera_config
-      Puppet[:hiera_config] = hiera_config
-    end
-  end
-
   c.before(:each) do
+    Puppet::Indirector::Hiera.stub(:hiera => hiera_stub)
     Puppet::Util::Log.level = :warning
     Puppet::Util::Log.newdestination(:console)
   end
+
 end
