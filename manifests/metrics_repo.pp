8dd7ac32a57dde7316214adbe8cae7f1dc3558e4
Brett Porter <brett@maestrodev.com>
2013-09-30 11:29:51 +1000

    make statsd optional

--- manifests/metrics_repo.pp
+++ manifests/metrics_repo.pp
@@ -1,27 +1,28 @@
-class maestro_nodes::metrics_repo{
+class maestro_nodes::metrics_repo($statsd_enabled = true) {
 
   class { 'mongodb':
     enable_10gen => true,
   }
 
-  $statsd_config = { 'mongoHost' => '"localhost"',
-    'mongoMax' => 2160
-  }
-  $backends =   [ 'mongo-statsd-backend' ]
+  if $statsd_enabled {
+    $statsd_config = { 'mongoHost' => '"localhost"',
+      'mongoMax' => 2160
+    }
+    $backends =   [ 'mongo-statsd-backend' ]
 
-  class { 'nodejs':
-  } ->
-  file { "/etc/yum.repos.d/nodejs-stable.repo":
-    ensure => absent,
+    class { 'nodejs':
+    } ->
+    file { "/etc/yum.repos.d/nodejs-stable.repo":
+      ensure => absent,
+    }
+    package { 'mongo-statsd-backend':
+      ensure   => present,
+      provider => npm,
+    } ->
+    class { 'statsd':
+      ensure   => '0.4.0',
+      backends => $backends,
+      config   => $statsd_config,
+    }
   }
-  package { 'mongo-statsd-backend':
-    ensure   => present,
-    provider => npm,
-  } ->
-  class { 'statsd':
-    ensure   => '0.4.0',
-    backends => $backends,
-    config   => $statsd_config,
-  }
-
 }
