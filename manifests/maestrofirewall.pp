cf36d40f925256462e970f7fff696f3b6f3adfa0
Carlos Sanchez <csanchez@maestrodev.com>
2013-12-13 09:22:00 +0100

    Split maestrofirewall into firewall and firewall::maestro

--- manifests/maestrofirewall.pp
+++ manifests/maestrofirewall.pp
@@ -1,28 +1,7 @@
 class maestro_nodes::maestrofirewall {
 
-  # Purge unmanaged firewall resources
-  #
-  # This will clear any existing rules, and make sure that only rules
-  # defined in puppet exist on the machine
-  resources { 'firewall':
-    purge => true,
-  }
+  notify { 'maestro_nodes::maestrofirewall is deprecated, use maestro_nodes::firewall::maestro': }
 
-  # ensure purge happens after basic firewall rules are set
-  # https://github.com/puppetlabs/puppetlabs-firewall/issues/239#issuecomment-26443579
+  include maestro_nodes::firewall::maestro
 
-  class{ ['maestro_nodes::firewall::pre', 'firewall', 'maestro_nodes::firewall::post']:
-    before => Resources['firewall'],
-  }
-
-  firewall { '020 allow http/https':
-    proto  => 'tcp',
-    port   => [80,443,8080],
-    action => 'accept',
-  }
-  firewall { '030 allow stomp':
-    proto  => 'tcp',
-    port   => [61613],
-    action => 'accept',
-  }
 }
