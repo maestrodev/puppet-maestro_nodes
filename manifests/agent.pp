80ec7892a4c713de71f6c7c3518c34a84e68dfea
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-10 21:51:30 +0100

    Add ssh:client to agent to manage ssh_known_hosts properly

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -45,6 +45,15 @@ class maestro_nodes::agent(
     type => 'ssh-rsa',
     key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==',
   }
+  # need to work around https://tickets.puppetlabs.com/browse/PUP-1177
+  # /etc/ssh/ssh_known_hosts has bad permissions
+  case $::kernel {
+    'Linux': {
+      class { 'ssh::client': }
+    }
+    default: {
+    }
+  }
 
   # Subversion
   class { 'svn': }
