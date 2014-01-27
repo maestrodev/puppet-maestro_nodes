532cf41577c4b72439f9b21e573fc296efabbf65
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-27 12:16:55 +0100

    Update maestro and rvm modules, use rvm::rvmrc

--- manifests/agentrvm.pp
+++ manifests/agentrvm.pp
@@ -10,16 +10,9 @@ class maestro_nodes::agentrvm(
   }
 
   # ensure rvm doesn't timeout finding binary rubies
-  # the umask line is the default content when installing rvm if file does not exist
-  file { '/etc/rvmrc':
-    content => 'umask u=rwx,g=rwx,o=rx
-                export rvm_max_time_flag=20',
-    mode    => '0664',
-    owner   => 'root',
-    group   => 'rvm',
-    before  => Class['rvm'],
+  class { 'rvm::rvmrc':
+    max_time_flag => 20,
   }
-
   class { 'rvm': }
 
   rvm::system_user { $agent_user:
